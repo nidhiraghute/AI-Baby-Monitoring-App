b692c8efff0ff181d8831d1537bf876e302bced0
nidhiraghute <85737394+nidhiraghute@users.noreply.github.com>
2022-03-29 16:43:14 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,63 +1,73 @@
-img = "";
-status = "";
+song = "";
 objects = [];
-baby = "";
+status = "";
 
-function setup(){
-    canvas = createCanvas(380, 380);
-    canvas.center();
-    video = createCapture(VIDEO);
-    video.size(380, 380);
-    video.hide();
-    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
-    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+function preload()
+{
+	song = loadSound("alarm.mp3");
 }
 
-function draw(){
-    image(video, 0, 0, 380, 380);
+function setup() {
+  canvas = createCanvas(380, 380);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.size(380,380);
+  video.hide();
+  objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+  document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
 
-    r = random(255);
-    g = random(255);
-    b = random(255);
+function modelLoaded() {
+  console.log("Model Loaded!")
+  status = true;
+}
 
-    objectDetector.detect(video, gotResult);
+function gotResult(error, results) {
+  if (error) {
+    console.log(error);
+  }
+  console.log(results);
+  objects = results;
+}
 
-    if(status != ""){
-        for(i = 0; i < objects.length; i++){
-     
-        document.getElementById("status").innerHTML = "Status: Object Detected";
-        document.getElementById("baby").innerHTML = "Number of Objects Detected are:"+ objects.length;
-      fill(r,g,b);
-      percent = floor(objects[i].confidence * 100);
-      text = objects[i].label + "" + percent + "%" ,objects[i].x + 15 ,objects[i].y + 15;
-      noFill();
-      stroke(r,g,b);
-      rect( objects[i].x ,objects[i].y ,objects[i].width ,objects[i].height);
-        }  
-    }
 
-    if(0 < objects.length){
-            document.getElementById("baby").innerHTML = "Baby Found";
-            
+function draw() {
+  image(video, 0, 0, 380, 380);
+      if(status != "")
+      {
+        r =  random(255);
+        g =  random(255);
+        b =  random(255);      
+        objectDetector.detect(video, gotResult);
+        for (i = 0; i < objects.length; i++) {
+          document.getElementById("status").innerHTML = "Status : Object Detected";
+ 
+          fill(r,g,b);
+          percent = floor(objects[i].confidence * 100);
+          text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+          noFill();
+          stroke(r,g,b);
+          rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+         
+          if(objects[i].label == "person")
+          {
+            document.getElementById("number_of_objects").innerHTML = "Baby Found";
+            console.log("stop");
+            song.stop();
+          }
+          else
+          {
+            document.getElementById("number_of_objects").innerHTML = "Baby Not Found";
+            console.log("play"); 
+            song.play();
+          }
+         }
+
+        if(objects.length == 0)
+        {
+          document.getElementById("number_of_objects").innerHTML = "Baby Not Found";
+          console.log("play"); 
+          song.play();
+        }
       }
-      else
-      document.getElementById("baby").innerHTML = "Baby Not Found";
-    
-}
-if( 0 > objects.length){
-    document.getElementById("baby").innerHTML = "Baby Not Found"; 
-}
-
-function modelLoaded(){
-    console.log("Model Loaded!")
-    status = true;
-    objectDetector.detect(video, gotResult);
-}
-
-function gotResult(error, results){
-    if(error){
-      console.log(error);
-    }
-    console.log(results);
-    objects = results;
 }
